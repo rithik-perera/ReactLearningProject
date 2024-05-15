c5c174b4fb0f84c7d28923d63c618f72a85fbc4b
rithik-perera <rithik.perera.2003@gmail.com>
2024-05-15 15:26:37 +0530

    added userGreetings which consists of conditional statements

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,33 +2,41 @@ import Button from "./Button/Button.jsx";
 import SearchBar from './SearchBar.jsx'
 import Student from "./Student/Student.jsx";
 import Sticker from "./Sticker/Sticker.jsx";
+import UserGreetings from "./UserGreetings.jsx";
 
 function App() {
 
-  return(
+  // //code for the sticker with props
+  // return(
 
-    <>
-      < Sticker 
-      title = 'Normal Book Sticker'
-      fname =  'Rithik'
-      lname = 'Perera'
-      class = '4LA'
-      subject = 'computer science' />
+  //   <>
+  //     < Sticker 
+  //     title = 'Normal Book Sticker'
+  //     fname =  'Rithik'
+  //     lname = 'Perera'
+  //     class = '4LA'
+  //     subject = 'computer science' />
 
-      <Sticker
-      title = 'Text book Sticker'
-      fname = 'Varun'
-      lname = 'Paul'
-      class = '3lb'
-      subject = 'software engineering'
-      />
+  //     <Sticker
+  //     title = 'Text book Sticker'
+  //     fname = 'Varun'
+  //     lname = 'Paul'
+  //     class = '3lb'
+  //     subject = 'software engineering'
+  //     />
 
-      <Sticker />
-      </> 
-      // this sticker apperars on the bottom,how to I adjuct the css such that this sticker appears on the left 
+  //     <Sticker />
+  //     </> 
+  //     // this sticker apperars on the bottom,how to I adjuct the css such that this sticker appears on the left 
    
-  );
+  // );
 
+  // // putting conditinal statements to functions
+  // return(
+  //   <UserGreetings loggedIn = {true} userName='RithikPerera' />
+  // );
+
+  
 }
 
 export default App;
