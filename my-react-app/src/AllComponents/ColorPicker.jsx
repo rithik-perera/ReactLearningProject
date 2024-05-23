189a2aa64a4f12c2ba2ab8a8bddfbbf06b65cf5b
rithik-perera <rithik.perera.2003@gmail.com>
2024-05-23 15:30:57 +0530

    completed the color picker

--- my-react-app/src/AllComponents/ColorPicker.jsx
+++ my-react-app/src/AllComponents/ColorPicker.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 function ColorPicker(){
 
-    [color, setColor] = useState('#fffff');
+    const [color, setColor] = useState('#fffff');
 
     const handleColorChange = (event)=>{
             setColor(event.target.value)
@@ -11,12 +11,23 @@ function ColorPicker(){
     return(
         <>
        <div className="color-picker">
-       <h2>Color Picker</h2>
+       <h2>Color Picker: </h2>
+       
+       <>
+            <div className="selected-color" style={{background: color}}>
+                <p>selected Color: <span style={{fontWeight: "bold"}}>{color}</span></p>
+            </div>
+            
             <input
                  type="color"
                  value={color}
                  onChange={handleColorChange}
                   />
+
+            
+        </>
+
+
        </div>
         </>
     );
